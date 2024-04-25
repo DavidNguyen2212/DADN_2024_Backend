d111c237e6fbdbef9032bd96c2bf3a98e55e8e3b
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-04-25 10:26:16 +0700

    Socket allow unsafe

--- src/run.py
+++ src/run.py
@@ -5,4 +5,4 @@ import sys,requests
 
 if __name__ == "__main__":
     # app.run(debug=True, use_debugger=True, use_reloader=False) 
-    socket_io.run(app)
+    socket_io.run(app, allow_unsafe_werkzeug=True)

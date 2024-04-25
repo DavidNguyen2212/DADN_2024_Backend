430cbc3c1c2905e8096d8ae1548e5c3aa969ad2b
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-04-25 11:47:03 +0700

    app

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from library import socket_io
 from library import connectIOserver     # connected! 
 import sys,requests
 
-this_app = app.app_context()
+this_app = app
 if __name__ == "__main__":
     # app.run(debug=True, use_debugger=True, use_reloader=False) 
     socket_io.run(app, allow_unsafe_werkzeug=True)

42395a7259fbff41809c5c58f2d5e0232f3a1476
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-04-25 11:34:59 +0700

    app

--- run.py
+++ run.py
@@ -6,6 +6,6 @@ import sys,requests
 this_app = app
 if __name__ == "__main__":
     # app.run(debug=True, use_debugger=True, use_reloader=False) 
-    # socket_io.run(app, allow_unsafe_werkzeug=True)
-    this_app = app
+    socket_io.run(app, allow_unsafe_werkzeug=True)
+    # this_app = app
     # app.run(debug=False)

dadd0cc757bd863dac7949af9f70e0dd1e3a93ca
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-04-25 15:17:56 +0700

    Fix circuler import

--- run.py
+++ run.py
@@ -9,6 +9,6 @@ import sys,requests
 this_app = app
 if __name__ == "__main__":
     # app.run(debug=True, use_debugger=True, use_reloader=False) 
-    socket_io.run(app, allow_unsafe_werkzeug=True)
+    socket_io.run(app, allow_unsafe_werkzeug=True, port=5000)
     # this_app = app
     # app.run(debug=False)

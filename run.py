0c1f8fb87513fc6075fe926f5c25f446981562b8
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-04-26 17:47:58 +0700

    Eventlet patch

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ import sys,requests
 import os
 
 this_app = app
-if __name__ == "__main__":
-    # app.run(debug=True, use_debugger=True, use_reloader=False) 
-    socket_io.run(app, port=os.environ.get("PORT"), log_output=True, debug=False)
+# if __name__ == "__main__":
+#     # app.run(debug=True, use_debugger=True, use_reloader=False) 
+#     socket_io.run(app, port=os.environ.get("PORT"), log_output=True, debug=False)

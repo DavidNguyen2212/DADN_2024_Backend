ff91c7d1a957de208bb84c819e3726890862eb3d
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-04-26 17:20:17 +0700

    Eventlet patch

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from library import connectIOserver     # connected!
 import sys,requests
 import os
 
-
+this_app = app
 if __name__ == "__main__":
     # app.run(debug=True, use_debugger=True, use_reloader=False) 
     socket_io.run(app, port=os.environ.get("PORT"), log_output=True, debug=False)

7c90fe66a9ac7033c90efc69811cb74ce70f5c90
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-04-28 22:55:58 +0700

    Fix connect

--- library/__init__.py
+++ library/__init__.py
@@ -113,8 +113,8 @@ client.on_subscribe = subscribe
 client.connect()
 print("Here on conect")
 complete_client = client
-# client.loop_background()  # Start the background loop for handling incoming messages
-client.loop()
+client.loop_background()  # Start the background loop for handling incoming messages
+# client.loop()
 
 from library.controllers import notificationsController
 from library.controllers import authController

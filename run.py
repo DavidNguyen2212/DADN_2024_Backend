a63e06bd29f987bfbd92b6c07025459df7698dc5
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-04-29 01:03:42 +0700

    Fix connect

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from library import socket_io
 this_app = app
 if __name__ == "__main__":
     # app.run(debug=True, use_debugger=True, use_reloader=False) 
-    socket_io.run(app)
+    socket_io.run(app, host="0.0.0.0", port=3107, allow_unsafe_werkzeug=True)

d9bbaf56341a1ea42a8aa39c5251bb8d165d4b38
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-04-28 11:44:28 +0700

    Try to fix socket

--- library/__init__.py
+++ library/__init__.py
@@ -24,7 +24,7 @@ def test_connect():
 mongo_client = PyMongo(app)
 db = mongo_client.db
 
-# from library import connectIOserver
+from library import connectIOserver
 from library.controllers import notificationsController
 from library.controllers import authController
 from library.controllers import refreshTokenController

ae4f65082f1a5e3e51c80ecfc43feb496f191486
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-04-28 21:14:25 +0700

    Fix connect

--- library/__init__.py
+++ library/__init__.py
@@ -28,9 +28,23 @@ db = mongo_client.db
 
 AIO_FEED_ID = ['temp', 'humi', 'light', 'chandeliers', 'control-fan','ac']
 
-from library.connectIOserver import create_Client
-client = create_Client()
-client.loop_background()
+# @mqtt.on_connect()
+# def handle_connect(client, userdata, flags, rc):
+topics = list(map(lambda x: "Giaqui14032002/feeds/" + x, AIO_FEED_ID))
+for topic in topics:
+    mqtt.subscribe(topic)
+    print("Subcribe thành công")
+
+# @mqtt.on_message()
+# def handle_mqtt_message(client, userdata, message):
+#     data = dict(
+#         topic=message.topic,
+#         payload=message.payload.decode()
+#     )
+#     # socket_io.emit('mqtt_message', data=data)
+#     print("Nhận dữ liệu từ ", str(message.topic), ": ", str(message.payload.decode()))
+mqtt.publish("Giaqui14032002/feeds/control-fan", "OFF")
+
 # from library import connectIOserver
 from library.controllers import notificationsController
 from library.controllers import authController

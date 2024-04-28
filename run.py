a840328e1a33644525421361fe15e22b5f9e7433
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-04-29 00:02:21 +0700

    Fix connect

--- run.py
+++ run.py
@@ -1,17 +1,6 @@
 from library import app
-# from library import client
-# from library import socket_io
-# from library import connectIOserver     # connected! 
 
-
-# client.connect()
-# print("Here")
-# client.loop_background()  
-
-# print("Here on conect")
-# complete_client = client
-# client.loop_background()  # Start the background loop for handling incoming messages
 this_app = app
-# if __name__ == "__main__":
-#     # app.run(debug=True, use_debugger=True, use_reloader=False) 
+if __name__ == "__main__":
+    app.run(debug=True, use_debugger=True, use_reloader=False) 
 #     socket_io.run(app, port=os.environ.get("PORT"), log_output=True, debug=False)

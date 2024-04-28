d52caf54b27a4ad95d7e26a1455656308f007e62
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-04-28 11:22:07 +0700

    Try to fix socket

--- library/controllers/authController.py
+++ library/controllers/authController.py
@@ -38,7 +38,7 @@ def login():
         # Thêm trường refresh token của người dùng trong cơ sở dữ liệu
         users.update_one({"_id": user_found["_id"]}, {"$set": {"refresh_token": refresh_token}})
         response = make_response(jsonify({"access_token": access_token}))
-        response.set_cookie('abcid', 'Hello', secure=True, samesite='None', max_age=63072000, domain="dadn-2024-team-blue-whale-1h2hw9qrf.vercel.app")
+        response.set_cookie('abcid', 'Hello', domain="dadn-2024-team-blue-whale-1h2hw9qrf.vercel.app", secure=True, samesite='None', max_age=63072000)
         set_refresh_cookies(response, refresh_token, max_age=24*60*60*1000)
         # print(jsonify(response))
         return response, 200

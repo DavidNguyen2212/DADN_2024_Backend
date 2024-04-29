335c2c9adbc7a613c8f5ea0e67e273c5a7ed304f
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-04-29 10:23:34 +0700

    Monkey patch

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from library import connectIOserver
 
 if __name__ == "__main__":
     # app.run(debug=True, use_debugger=True, use_reloader=False) 
-    socket_io.run(app, host="0.0.0.0", port=3017,log_output=True, allow_unsafe_werkzeug=True)
+    socket_io.run(app, port=3017,log_output=True, allow_unsafe_werkzeug=True)

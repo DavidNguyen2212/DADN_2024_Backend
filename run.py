9c50eb96ac628fd85d7e37a60d746866d667f8b3
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-04-29 12:30:26 +0700

    Monkey patch

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ import eventlet
 
 if __name__ == "__main__":
     # app.run(debug=True, use_debugger=True, use_reloader=False) 
-    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+    eventlet.wsgi.server(eventlet.listen(('', os.environ.get("PORT"))), app)
     # socket_io.run(app, host="0.0.0.0", port=os.environ.get("PORT"), allow_unsafe_werkzeug=True)

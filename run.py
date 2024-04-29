05a5481d2c66d3ece7974d16542d7dd07b4598a9
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-04-29 09:46:22 +0700

    Prepare for final deployment

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from library import app, socket_io
 
 if __name__ == "__main__":
     # app.run(debug=True, use_debugger=True, use_reloader=False) 
-    socket_io.run(app, host="0.0.0.0", port=5007, allow_unsafe_werkzeug=True)
+    socket_io.run(app, allow_unsafe_werkzeug=True)
